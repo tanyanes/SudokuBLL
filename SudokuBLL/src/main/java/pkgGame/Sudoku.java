97d8d3ae7812ba925826688a8e19c22ff87120dc
TNest <TNest@128.4.101.139>
2018-09-24 19:27:57 -0400

    commit lab 2

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -1,18 +1,115 @@
 package pkgGame;
 
+
 import pkgHelper.LatinSquare;
 
 public class Sudoku extends LatinSquare {
-	private int[][] puzzle;
+	private int iSize;
+	private int iSqrtSize;
 	
 	public Sudoku() {
+		super();
+	}
+	
+	public Sudoku(int iSize) throws java.lang.Exception {
+		double root = Math.sqrt(iSize);
+		int temp = (int) root;
+		
+		if(Math.pow(root, 2) == Math.pow(temp, 2)) {
+			this.iSize = iSize;
+			this.iSqrtSize = (int) Math.sqrt(iSize); 
+		}
+		else {
+			throw new Exception("Invalid size");
+		}
+	}
+	
+	public Sudoku(int[][] puzzle) throws java.lang.Exception {
+		super(puzzle);
+		
+		double root = Math.sqrt(puzzle.length);
+		int temp = (int) root;
+		
+		if(Math.pow(root, 2) == Math.pow(temp, 2)) {
+			iSize = puzzle.length;
+			iSqrtSize = (int) Math.sqrt(puzzle.length);
+		}
+		else {
+			throw new Exception("Invalid size");
+		}
+	}
+	
+	public int[][] getPuzzle() {
+		return getLatinSquare();
+	}
+	
+	public int[] getRegion(int r) {
+		int[] reg = new int[getLatinSquare().length];
+		
+		int i = (r % iSqrtSize) * iSqrtSize;
+		int j = (r / iSqrtSize) * iSqrtSize;
+		int iMax = i + iSqrtSize;
+		int jMax = j + iSqrtSize;
+		int index = 0;
+		
+		for(; j < jMax; j++) {
+			for(i = (r % iSqrtSize) * iSqrtSize; i < iMax; i++) {
+				reg[index] = getLatinSquare()[j][i];
+				index++;
+			}
+		}
+		return reg;
+	}
+	
+	public int[] getRegion(int iCol, int iRow) {
+		return getRegion((iCol/iSqrtSize) + ((iRow/iSqrtSize)*iSqrtSize)); 
+	}
+	
+	public boolean isPartialSudoku() {
+		int[] firstRow = getRow(0);
+		int[] temp;
+		
+		if(!this.isLatinSquare()) {
+			if(ContainsZero()) {
+				return true;
+			}
+			return false;
+		}
+		
+		for(int j = 0; j < iSize; j++) {
+			temp = getRegion(j);
+			
+			if(hasDuplicates(temp)) {
+				return false;
+			}
+			if (!ContainsZero()) {
+				return false;
+			}
+			if(!(hasAllValues(temp, firstRow))) {
+				if(ContainsZero()) {
+					return true;
+				}
+				return false;
+			}
+		}
+		
+		return true;
 		
 	}
 	
-	@Override
-	public boolean hasDuplicates(int[] arr) throws Exception {
-
-		//CODE THAT CAN CAUSE ERROR
-		
+	public boolean isSudoku() {
+		return isPartialSudoku() && !ContainsZero();
+	}
+	
+	public boolean isValidValue(int iCol, int iRow, int iValue) {
+		boolean test = false;
+		if(iValue <= iSize && iValue >= 0) {
+			if(!(doesElementExist(getColumn(iCol), iValue))) {
+				if(!(doesElementExist(getRow(iRow), iValue))) {
+					test = true;
+				}
+			}
+		}
+		return test;
 	}
 }
---
+++ SudokuBLL/src/test/java/pkgGame/SudokuTest.java
@@ -0,0 +1,225 @@
+package pkgGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class SudokuTest {
+	
+		@Test
+		public void testGetRegionInt_test1() throws Exception {
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			int[] ret = temp.getRegion(0);
+			int[] result = {7,9,1,6,8,5,3,2,4};
+			assertTrue(Arrays.equals(ret, result));
+		}
+		
+		@Test
+		public void testGetRegionInt_test2() throws Exception {
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			int[] ret = temp.getRegion(8);
+			int[] result = {1,4,2,6,9,7,8,5,3};
+			assertTrue(Arrays.equals(ret, result));
+		}
+
+		@Test
+		public void testGetRegionIntInt_test1() throws Exception {
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			int[] ret = temp.getRegion(0, 0);
+			int[] result = {7,9,1,6,8,5,3,2,4};
+			assertTrue(Arrays.equals(ret, result));
+		}
+		
+		@Test
+		public void testGetRegionIntInt_test2() throws Exception {
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			int[] ret = temp.getRegion(3, 4);
+			int[] result = {3,1,5,4,6,2,9,8,7};
+			assertTrue(Arrays.equals(ret, result));
+		}
+
+		@Test
+		public void testIsPartialSudoku_test1() throws Exception{ //baseline test
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			assertFalse(temp.isPartialSudoku());
+		}
+		
+		@Test
+		public void testIsPartialSudoku_test2() throws Exception { //test with 0
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,0,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,0,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			assertTrue(temp.isPartialSudoku());
+		}
+		
+		@Test
+		public void testIsPartialSudoku_test3() throws Exception { // test for duplicate
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,7,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			assertFalse(temp.isPartialSudoku());
+		}
+		
+
+		@Test
+		public void testIsSudoku_test1() throws Exception {
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			assertFalse(temp.isSudoku());
+			assertFalse(temp.isPartialSudoku());
+		}
+		
+		@Test
+		public void testIsSudoku_test2() throws Exception {
+			int[][] test = {
+					{0,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			assertFalse(temp.isSudoku());
+		}
+		
+		@Test
+		public void testIsSudoku_test3() throws Exception {
+			int[][] test = {
+					{7,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,7,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			assertFalse(temp.isSudoku());
+		}
+
+		@Test
+		public void testIsValidValue_test1() throws Exception {
+			int[][] test = {
+					{0,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			assertTrue(temp.isValidValue(0, 0, 7));
+		}
+		
+		@Test
+		public void testIsValidValue_test2() throws Exception {
+			int[][] test = {
+					{0,9,1,8,5,6,3,2,4},
+					{6,8,5,2,4,3,7,1,9},
+					{3,2,4,1,7,9,5,8,6},
+					{9,7,2,3,1,5,4,6,8},
+					{8,5,3,4,6,2,9,7,1},
+					{1,4,6,9,8,7,2,3,5},
+					{5,6,9,7,3,8,1,4,2},
+					{4,3,8,5,2,1,6,9,7},
+					{2,1,7,6,9,4,8,5,3}
+			};
+			Sudoku temp = new Sudoku(test);
+			assertFalse(temp.isValidValue(0, 0, 6));
+			
+		}
+}
